a95e08ee0b31dc580c3274e412cce4474a2b62ab
Varga Gergely <reapedsoul@gmail.com>
2018-02-18 16:33:00 +0100

    feat(Exercise_35_DrawChesstable impr by taking user input size

--- week-01/day-4-5/src/Exercises_35_DrawChessTable.java
+++ week-01/day-4-5/src/Exercises_35_DrawChessTable.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Exercises_35_DrawChessTable {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Crate a program that draws a chess table like this
         //
         // % % % %
@@ -11,12 +13,21 @@ public class Exercises_35_DrawChessTable {
         // % % % %
         //  % % % %
         //
-        String chessPattern = "% % % %";
-        for (int i = 0; i < 4; i++){
-            System.out.println(chessPattern);
-            System.out.println(" " + chessPattern);
-
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfTable = scanner.nextInt();
+        for (int rows = 1; rows <= sizeOfTable; rows++) {
+            for (int columns = 0; columns <= sizeOfTable; columns++) {
+                if (columns % 2 == 0) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            if (rows % 2 == 0) {
+                System.out.println();
+            } else {
+                System.out.print("\n ");
+            }
         }
-
     }
 }
