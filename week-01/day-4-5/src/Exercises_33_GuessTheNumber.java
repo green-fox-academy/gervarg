464963f4f2def0a307861a8c9457fad90029da47
Varga Gergely <reapedsoul@gmail.com>
2018-02-18 17:25:58 +0100

    feat(feature): GuessTheNumber added RNG number to guess

--- week-01/day-4-5/src/Exercises_33_GuessTheNumber.java
+++ week-01/day-4-5/src/Exercises_33_GuessTheNumber.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Exercises_33_GuessTheNumber {
+    static int storedNumber = (int)(Math.random() * 100 + 1);
     public static void main(String[] args) {
-        int storedNumber = 8;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Guess the number: ");
+        System.out.print("Guess the number between 1-100: ");
         int guessNumber = scanner.nextInt();
         // Write a program that stores a number, and the user has to figure it out.
         // The user can input guesses, after each guess the program would tell one
@@ -26,3 +27,6 @@ public class Exercises_33_GuessTheNumber {
         System.out.println("You found the number: " + guessNumber);
     }
 }
+
+
+
