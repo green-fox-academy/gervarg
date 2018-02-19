365954eb1a76bfab41cedd4eb4a252d308399527
Varga Gergely <reapedsoul@gmail.com>
2018-02-19 20:21:31 +0100

    feat(w02-d2-homework): Unique solved

--- week-02/day-1/src/Exercise_20_Unique.java
+++ week-02/day-1/src/Exercise_20_Unique.java
@@ -1,28 +1,37 @@
+import java.util.Arrays;
 
 //still trying to solve this problem
 public class Exercise_20_Unique {
     public static void main(String[] args){
                 //  Create a function that takes a list of numbers as a parameter
                 //  Returns a list of numbers where every number in the list occurs only once
-
                 //  Example
                 System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
                 //  should print: `[1, 11, 34, 52, 61]`
     }
-    public static int[] unique(int[] numberList){
-        int testNumber = numberList[0];
+    public static String unique(int[] numberList) {
+        int testNumber;
         int counter = 0;
         int[] uniqueArray = new int[numberList.length];
         for (int i = 0; i < numberList.length; i++) {
-            if (numberList[i] == testNumber) {
-                counter++;
-            } else {
-                for (int j = 0; j < numberList.length-counter ; j++) {
-                    uniqueArray[j] = numberList[i];
-                    testNumber = numberList[i];
+            testNumber = numberList[i];
+            int tester = 0;
+            for (int j = 0; j < numberList.length; j++) {
+                if (uniqueArray[j] != testNumber) {
+                    tester++;
                 }
             }
+            if (tester == numberList.length) {
+                uniqueArray[counter] = testNumber;
+                counter++;
+            }
         }
-        return uniqueArray;
+        int[] uniqueArrayFinal = new int[counter];
+        for (int k = 0; k < counter; k++) {
+            uniqueArrayFinal[k] = uniqueArray[k];
+            System.out.println(uniqueArrayFinal[k]);
+        }
+        return Arrays.toString(uniqueArrayFinal);
     }
 }
+
