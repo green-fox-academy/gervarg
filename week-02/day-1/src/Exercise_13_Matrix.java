e009d151899f4b6d7e60805e0e597d734edbab1e
Varga Gergely <reapedsoul@gmail.com>
2018-02-19 16:14:38 +0100

    feat(w02-d1): Exercises Arrays until ReverseList Done

---
+++ week-02/day-1/src/Exercise_13_Matrix.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise_13_Matrix {
+    public static void main(String[] args){
+        // - Create (dynamically) a two dimensional array
+        //   with the following matrix. Use a loop!
+        //
+        //   1 0 0 0
+        //   0 1 0 0
+        //   0 0 1 0
+        //   0 0 0 1
+        //
+        // - Print this two dimensional array to the output
+        Scanner userInput = new Scanner(System.in);
+        int matrixSize = userInput.nextInt();
+        int[][] matrix = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if ( i == j){
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
