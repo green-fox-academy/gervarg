04fb46ffc7dcf6a40526bd797b78181d75c1bf77
Varga Gergely <reapedsoul@gmail.com>
2018-02-20 12:39:07 +0100

    feat(w02-d2): ToDoPrint, Reverse done

---
+++ week-02/day-2/src/Exercise_05_Reverse.java
@@ -0,0 +1,18 @@
+public class Exercise_05_Reverse {
+    public static void main(String[] args) {
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        System.out.println(reverse(reversed));
+    }
+    public static String reverse(String stringToReverse){
+        String tempString = "";
+        for (int i = stringToReverse.length()-1; i >= 0 ; i--) {
+            tempString += stringToReverse.charAt(i);
+        }
+        return tempString;
+    }
+}
