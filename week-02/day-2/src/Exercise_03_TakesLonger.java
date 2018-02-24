1f5ae2819600b7e8f8ba7f82d8dd1c32fc896804
Varga Gergely <reapedsoul@gmail.com>
2018-02-24 16:59:49 +0100

    fix(Refactor): Refactoring some Exercises

--- week-02/day-2/src/Exercise_03_TakesLonger.java
+++ week-02/day-2/src/Exercise_03_TakesLonger.java
@@ -5,8 +5,12 @@ public class Exercise_03_TakesLonger {
         // When saving this quote a disk error has occured. Please fix it.
         // Add "always takes longer than" to the String(quote) between the words "It" and "you"
         // Using pieces of the quote variable (instead of just redefining the string)
-        System.out.println(quote.indexOf("It"));
-        quote = quote.substring(0, 21) + "always takes longer than " + quote.substring(21,quote.length());
-        System.out.println(quote);
+      System.out.println(quote.indexOf("It"));
+      System.out.println(swapWords(quote));
+
+    }
+    public static String swapWords(String quoteToFix){
+      quoteToFix = quoteToFix.substring(0, 21) + "always takes longer than " + quoteToFix.substring(21,quoteToFix.length());
+      return quoteToFix;
     }
 }
