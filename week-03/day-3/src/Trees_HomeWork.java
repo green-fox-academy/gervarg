8699b6e997e4d7a905ec7161cb5e2a842b96c7e9
Varga Gergely <reapedsoul@gmail.com>
2018-02-27 21:21:16 +0100

    fix(Homework): hashmaps corrected to the right style

--- week-03/day-3/src/Trees_HomeWork.java
+++ week-03/day-3/src/Trees_HomeWork.java
@@ -3,34 +3,35 @@ import java.util.HashMap;
 public class Trees_HomeWork {
   public static void main(String[] args) {
 
-    HashMap<String, String> treeNames = new HashMap<String, String >();
-    treeNames.put("tree01", "oak");
-    treeNames.put("tree02", "pine");
-    treeNames.put("tree03", "aspen");
-    treeNames.put("tree04", "beech");
-    treeNames.put("tree05", "willow-tree");
+    HashMap<String, Object> treeType1 = new HashMap<String, Object>();
+    treeType1.put("type", "oak");
+    treeType1.put("color", "green");
+    treeType1.put("age", 25);
+    treeType1.put("sex", "male");
 
+    HashMap<String, Object> treeType2 = new HashMap<String, Object>();
+    treeType2.put("tree01", "pine");
+    treeType2.put("color", "dark green");
+    treeType2.put("age", 80);
+    treeType2.put("sex", "male");
 
-    HashMap<String, String> treeColors = new HashMap<String, String >();
-    treeColors.put("tree01", "yellow");
-    treeColors.put("tree02", "brown");
-    treeColors.put("tree03", "green");
-    treeColors.put("tree04", "red");
-    treeColors.put("tree05", "light-green");
+    HashMap<String, Object> treeType3 = new HashMap<String, Object>();
+    treeType3.put("tree01", "aspen");
+    treeType3.put("color", "yellow");
+    treeType3.put("age", 40);
+    treeType3.put("sex", "female");
 
-    HashMap<String, Integer> treeAge = new HashMap<String, Integer>();
-    treeAge.put("tree01", 25);
-    treeAge.put("tree02", 50);
-    treeAge.put("tree03", 70);
-    treeAge.put("tree04", 80);
-    treeAge.put("tree05", 100);
+    HashMap<String, Object> treeType4 = new HashMap<String, Object>();
+    treeType4.put("tree01", "beech");
+    treeType4.put("color", "orange");
+    treeType4.put("age", 80);
+    treeType4.put("sex", "female");
 
-    HashMap<String, String> treeSex = new HashMap<String, String >();
-    treeSex.put("tree01", "male");
-    treeSex.put("tree02", "female");
-    treeSex.put("tree03", "male");
-    treeSex.put("tree04", "female");
-    treeSex.put("tree05", "male");
+    HashMap<String, Object> treeType5 = new HashMap<String, Object>();
+    treeType5.put("type", "willow-tree");
+    treeType5.put("color", "light-green");
+    treeType5.put("age", 30);
+    treeType5.put("sex", "male");
 
   }
 }
