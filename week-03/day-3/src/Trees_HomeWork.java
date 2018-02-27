abec06081df5098c5d8c3b59ba1ffb94c20f02dd
Varga Gergely <reapedsoul@gmail.com>
2018-02-27 19:27:02 +0100

    feat(Homework): trees done

---
+++ week-03/day-3/src/Trees_HomeWork.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class Trees_HomeWork {
+  public static void main(String[] args) {
+
+    HashMap<String, String> treeNames = new HashMap<String, String >();
+    treeNames.put("tree01", "oak");
+    treeNames.put("tree02", "pine");
+    treeNames.put("tree03", "aspen");
+    treeNames.put("tree04", "beech");
+    treeNames.put("tree05", "willow-tree");
+
+
+    HashMap<String, String> treeColors = new HashMap<String, String >();
+    treeColors.put("tree01", "yellow");
+    treeColors.put("tree02", "brown");
+    treeColors.put("tree03", "green");
+    treeColors.put("tree04", "red");
+    treeColors.put("tree05", "light-green");
+
+    HashMap<String, Integer> treeAge = new HashMap<String, Integer>();
+    treeAge.put("tree01", 25);
+    treeAge.put("tree02", 50);
+    treeAge.put("tree03", 70);
+    treeAge.put("tree04", 80);
+    treeAge.put("tree05", 100);
+
+    HashMap<String, String> treeSex = new HashMap<String, String >();
+    treeSex.put("tree01", "male");
+    treeSex.put("tree02", "female");
+    treeSex.put("tree03", "male");
+    treeSex.put("tree04", "female");
+    treeSex.put("tree05", "male");
+
+  }
+}
