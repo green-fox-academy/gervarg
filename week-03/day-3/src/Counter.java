8c46292425ae7296108e2de833c609adbec86bf7
Varga Gergely <reapedsoul@gmail.com>
2018-02-28 15:04:01 +0100

    feat(W03-D3): counter with test done

---
+++ week-03/day-3/src/Counter.java
@@ -0,0 +1,26 @@
+public class Counter {
+  private int value = 0;
+  private int startValue = 0;
+
+  public Counter(){
+
+  }
+
+  public Counter(int value){
+    this.value= value;
+  }
+
+  public void add(int number){
+    value += number;
+  }
+  public void add(){
+    value++;
+  }
+  public int get(){
+    return value;
+  }
+  public void reset(){
+    value = 0;
+  }
+
+}
