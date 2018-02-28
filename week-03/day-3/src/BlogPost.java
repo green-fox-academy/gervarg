16600f492fae937fbe974b9c859837d93db6998d
Varga Gergely <reapedsoul@gmail.com>
2018-02-28 11:39:12 +0100

    feat(W03-D3): blogPost class done, and created objects in Main

---
+++ week-03/day-3/src/BlogPost.java
@@ -0,0 +1,15 @@
+public class BlogPost {
+  private String authorname;
+  private String title;
+  private String text;
+  private String publicationDate;
+
+  public BlogPost(String authorname, String title, String text, String publicationDate){
+    this.authorname = authorname;
+    this.title = title;
+    this.text = text;
+    this. publicationDate = publicationDate;
+  }
+
+
+}
--- week-03/day-3/src/Main.java
+++ week-03/day-3/src/Main.java
@@ -4,6 +4,16 @@ public class Main {
     PostIt pink = new PostIt("pink", "Awesome", "black");
     PostIt yellow = new PostIt("yellow", "Superb!", "green");
 
+    BlogPost latin = new BlogPost("John Doe", "Lorem ipsum", "Lorem ipsum dolor sit amet.",
+            "2000.05.04.");
+    BlogPost  waitButWhy = new BlogPost("Tim Urban", "Wait but why",
+            "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+    BlogPost engineerToIBM = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump",
+            "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. " +
+                    "When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t " +
+                    "really into the whole organizer profile thing.", "2017.03.28.");
+
+
 
   }
 }
