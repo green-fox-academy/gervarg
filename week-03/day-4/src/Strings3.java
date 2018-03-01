08566b03e27e7cff98e9fae756b5931f47128a44
Varga Gergely <reapedsoul@gmail.com>
2018-03-01 12:28:26 +0100

    feat(W03-D4): fibonacci done

---
+++ week-03/day-4/src/Strings3.java
@@ -0,0 +1,16 @@
+public class Strings3 {
+  public static void main(String[] args) {
+    // Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a "*".
+ /*   String word = "tellmeaboutit";
+    System.out.println(addStarsToString(word, "*"));
+  }
+
+  private static String addStarsToString(String word, String s) {
+    if () {
+      return word;
+    } else {
+      return addStarsToString(word., s);
+    }*/ //work in progress
+  }
+}
