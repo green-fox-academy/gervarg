615829f71ba2e7976bd527da5dfd1283a3bedc75
Varga Gergely <reapedsoul@gmail.com>
2018-03-02 12:39:15 +0100

    feat(Sierpinsky): added animation

--- week-03/day-5/src/Sierpinsky.java
+++ week-03/day-5/src/Sierpinsky.java
@@ -7,14 +7,15 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Sierpinsky {
   static int LIMIT = 7;
-  static int iterator = 0;
+  static int ITERATOR = 0;
   public static void mainDraw(Graphics graphics){
 
     int size = WIDTH/3;
     int x = WIDTH/2-size/2;
     int y = HEIGHT/2-size/2;
     int n = 0;
-    sierpinsky(x,y,graphics, n,size);
+    int iterator = 0;
+    sierpinsky(x,y,graphics, n,size, iterator);
     graphics.fillRect(x,y,size,size);
 
   }
@@ -27,30 +28,30 @@ public class Sierpinsky {
 
   }
 
-  private static void sierpinsky(int x, int y, Graphics g, int n, int size){
+  private static void sierpinsky(int x, int y, Graphics g, int n, int size, int iterator){
     g.setColor(getRandomColor());
     g.fillRect(x,y,size,size);
     if ( n == LIMIT ) {
       return;
-    } else {
+    } else if (iterator < ITERATOR) {
       n++;
       size /= 3;
       //top left
-      sierpinsky(x-2*size,y-2*size,g,n,size);
+      sierpinsky(x-2*size,y-2*size,g,n,size, iterator+5);
       //left
-      sierpinsky(x-2*size,y+size,g,n,size);
+      sierpinsky(x-2*size,y+size,g,n,size, iterator+10);
       //top mid
-      sierpinsky(x+size,y-2*size,g,n,size);
+      sierpinsky(x+size,y-2*size,g,n,size,iterator+5);
       //right
-      sierpinsky(x+4*size,y+size,g,n,size);
+      sierpinsky(x+4*size,y+size,g,n,size,iterator+10);
       //bottom right
-      sierpinsky(x+4*size,y+4*size,g,n,size);
+      sierpinsky(x+4*size,y+4*size,g,n,size,iterator+15);
       //bottom mid
-      sierpinsky(x+size,y+4*size,g,n,size);
+      sierpinsky(x+size,y+4*size,g,n,size,iterator+10);
       //bottom left
-      sierpinsky(x-2*size,y+4*size,g,n,size);
+      sierpinsky(x-2*size,y+4*size,g,n,size, iterator+15);
       //top right
-      sierpinsky(x+4*size,y-2*size,g,n,size);
+      sierpinsky(x+4*size,y-2*size,g,n,size,iterator+5);
 
 
 
@@ -63,10 +64,9 @@ public class Sierpinsky {
   static int WIDTH = 729;
   static int HEIGHT = 729;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     JFrame jFrame = new JFrame("Drawing");
     ImagePanel imagePanel = new ImagePanel();
-
     jFrame.setSize(new Dimension(WIDTH, HEIGHT));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(imagePanel);
@@ -74,7 +74,8 @@ public class Sierpinsky {
     jFrame.setVisible(true);
     while(true) {
       imagePanel.repaint();
-      iterator ++;
+      ITERATOR++;
+      Thread.sleep(300);
 
     }
   }
