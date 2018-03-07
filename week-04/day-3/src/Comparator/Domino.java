7fce2ecc1b92cc78a89661f57113c40118337917
Varga Gergely <reapedsoul@gmail.com>
2018-03-07 12:48:27 +0100

    feat(Comparable): comparable done

---
+++ week-04/day-3/src/Comparator/Domino.java
@@ -0,0 +1,29 @@
+package Comparator;
+
+import java.util.Arrays;
+
+public class Domino implements Comparable<Domino> {
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+
+  @Override
+  public int compareTo(Domino o) {
+    if (values[0] > o.values[0] ){
+      return values[0];
+    } else {
+      return -1;
+    }
+  }
+}
