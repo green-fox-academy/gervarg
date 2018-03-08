dcd8e0beaf9b779a06a45ecf7230f32075a85174
Varga Gergely <reapedsoul@gmail.com>
2018-03-08 15:13:13 +0100

    feat(CountLetters): created test for whitespaces

--- week-04/day-4/test/CountLettersTest.java
+++ week-04/day-4/test/CountLettersTest.java
@@ -1,13 +1,19 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CountLettersTest {
   CountLetters letters = new CountLetters();
 
   @Test
-  public void countLetters() {
+  public void countLettersTestForLetter() {
     String text = "Let there be anarchy";
-    assertEquals((Integer)4,letters.countLetters(text).get((Character)'e'));
+    assertEquals((Integer) 4, letters.countLetters(text).get((Character) 'e'));
+  }
+
+  @Test
+  public void countLettersTestWithWhiteSpace() {
+    String text = "Let there be anarchy";
+    assertEquals((Integer) 3, letters.countLetters(text).get((Character) ' '));
   }
 }
