44241a0834a67911410d574b6c6397be79c6e9ab
Varga Gergely <reapedsoul@gmail.com>
2018-03-11 15:46:14 +0100

    feat(Reservationy): reservation class finished giving random code

--- week-04/day-3/src/reservations/Reservation.java
+++ week-04/day-3/src/reservations/Reservation.java
@@ -5,24 +5,26 @@ import java.util.Random;
 public class Reservation implements Reservationy {
   private String[] daysOfWeek = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
   private Random random = new Random();
-  private char[] alphabetAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+  private char[] alphabetAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789".toUpperCase().toCharArray();
 
 
   @Override
   public String getDowBooking() {
-    int rngDay = random.nextInt(7);
+    int rngDay = random.nextInt(daysOfWeek.length);
     return  daysOfWeek[rngDay];
   }
 
   @Override
   public String getCodeBooking() {
+    StringBuilder stringBuilder = new StringBuilder();
     for (int i = 0; i < 8; i++) {
-
+      int rngCode = random.nextInt(alphabetAndNumbers.length);
+      stringBuilder.append(alphabetAndNumbers[rngCode]);
     }
-    return null;
+    return stringBuilder.toString();
   }
 
   public void reservationLine(){
-    System.out.println("Booking# " + getCodeBooking() + "for " + getDowBooking());
+    System.out.println("Booking# " + getCodeBooking() + " for " + getDowBooking());
   }
 }
