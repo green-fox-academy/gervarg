eb2bdd1bdabeb3b099f93f859b85755b25756b6b
Varga Gergely <reapedsoul@gmail.com>
2018-03-16 18:20:45 +0100

    feat(EncodedFile): encodedFile done

---
+++ week-03/day-2/src/Exercise_12_EncodedLines.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise_12_EncodedLines {
+  public static void main(String[] args) {
+    List<String> decodedMessage = decodeFile("encoded-lines.txt");
+    writeDecodedText("decodedFile.txt", decodedMessage);
+
+  }
+
+  private static List<String> decodeFile(String fileName) {
+    List<String> encodedText = fileRead(fileName); // i need ansi value -1
+    String lines;
+    String decodedLines = "";
+    for (int i = 0; i < encodedText.size(); i++) {
+      lines = encodedText.get(i);
+      for (int j = 0; j < lines.length(); j++) {
+        if (lines.charAt(j) != 32) {
+          decodedLines += (char) (lines.charAt(j) - 1);
+        } else {
+          decodedLines += " ";
+        }
+      }
+      encodedText.set(i, decodedLines);
+      decodedLines = "";
+    }
+    return encodedText;
+  }
+
+  private static List<String> fileRead(String fileName) {
+    Path filePath = Paths.get(fileName);
+    List<String> text = new ArrayList<>();
+    try {
+      text = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      System.out.println("Could not read file");
+    }
+    return text;
+  }
+
+  private static void writeDecodedText(String newFile, List<String> messageToWrite) {
+    Path filePath = Paths.get(newFile);
+    try {
+      Files.write(filePath, messageToWrite);
+    } catch (IOException e) {
+      System.out.println("Could not write to file");
+    }
+  }
+}
