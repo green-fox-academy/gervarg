3e4ae6812e86e24c8a4c95caad00806230b38a6b
Varga Gergely <reapedsoul@gmail.com>
2018-03-28 15:36:50 +0200

    feat(sentence): done but not working...

---
+++ week-06/day-3/sentence/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+// create a function which takes a char array as a parameter,
+// and splits a string to words by space
+// solve the problem with the proper string.h function
+
+void sentence(char*);
+
+int main()
+{
+    //i just don't get it why this doesn't work
+    printf("Type in a sentence:\n\n");
+    char string[256] = "why this not work";
+    //fgets(string, 256, stdin);
+    sentence(string);
+    return(0);
+}
+
+void sentence(char *string)
+{
+    char *word = strtok(string, " ");
+    while (word != NULL) {
+        printf("%s\n", word);
+        word = (NULL, " ");
+    }
+}
