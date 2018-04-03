166b93c0dcc845968a73e0378ea7403b5fa49932
Varga Gergely <reapedsoul@gmail.com>
2018-04-03 10:52:31 +0200

    feat(PI): exercise done

---
+++ week-06/day-2/Pi/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+
+// define a variable called PI with the value of 3.14
+// create a function which takes the radius of a circle as a parameter
+// and return the area of that cirle
+// area = radius * radius * PI
+
+float circle_area(int radius)
+{
+    return radius * radius * PI;
+}
+
+int main()
+{
+    int radius = 3;
+    printf("%.2f", circle_area(radius));
+	return 0;
+}
