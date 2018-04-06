2f5511b77c900d7ec1dc7ed3ef8fe2e1db2c4786
Varga Gergely <reapedsoul@gmail.com>
2018-04-06 12:18:59 +0200

    feat: fileread and filewrite functions created, sorting data still not working

--- week-07/day-4/temp_logger/BlackBox/parser.c
+++ week-07/day-4/temp_logger/BlackBox/parser.c
@@ -116,18 +116,74 @@ int log_data()
 	if (get_line_from_port(buff, PORT_BUFFER_LEN) > 0) {
 
         static int counter = 0;
-        data = (char**)realloc(data, (counter+1)*sizeof(*data));
-        data[counter] = (char*)malloc(sizeof(buff));
 
-        strcpy(data[counter], buff);
-        printf("%s\n", data[counter]);
-        counter++;
+        char *temp = (char*) malloc(sizeof(buff));
+        strcpy(temp, buff);
+        char *ptr_filter = strtok(buff, ".");
+        char *year_ptr = (char*)malloc(sizeof(5));
+        year_ptr = ptr_filter;
+        char *month_ptr = (char*)malloc(sizeof(2));
+        char *day_ptr = (char*)malloc(sizeof(2));
+        char *hour_ptr = (char*)malloc(sizeof(2));
+        char *minute_ptr = (char*)malloc(sizeof(2));
+        char *second_ptr = (char*)malloc(sizeof(2));
+        char *temp_ptr = (char*)malloc(sizeof(6));
+        long month;
+        long day;
+        long hour;
+        long minute;
+        long second;
+        int temperature_length;
+        for (int i = 0; i < 6; i++){
+            ptr_filter = strtok(NULL, ".: ");
+            if (i == 0) {
+                month_ptr = ptr_filter;
+                month = strtol(month_ptr, NULL, 10);
+                free(month_ptr);
+            }
+            else if (i == 1){
+                day_ptr = ptr_filter;
+                day = strtol(day_ptr, NULL, 10);
+                free(day_ptr);
+            }
+            else if (i == 2){
+                hour_ptr = ptr_filter;
+                hour = strtol(hour, NULL, 10);
+                free(hour_ptr);
+            }
+            else if (i == 3){
+                minute_ptr = ptr_filter;
+                minute = strtol(minute_ptr, NULL, 10);
+                free(minute_ptr);
+            }
+            else if (i == 4) {
+                second_ptr = ptr_filter;
+                second = strtol(second_ptr, NULL, 10);
+                free(second_ptr);
+            }
+            else {
+                temp_ptr = ptr_filter;
+                temperature_length = strlen(temp_ptr);
+                free(temp_ptr);
+            }
+
+        }
+        if (month > 0 && month < 12 && day > 0 && day < 32 && hour >= 0 && hour < 25 && minute >= 0 && minute < 60 && second >= 0 && second < 60 && temperature_length < 4){
+            printf("%s\n",temp);
+            data = (char**)realloc(data, (counter+1)*sizeof(*data));
+            data[counter] = (char*)malloc(sizeof(buff));
+            strcpy(data[counter], buff);
+            counter++;
+        }
+            //printf("%s\n",temp);
+        free(year_ptr);
+        free(temp);
+
         // Put the data into the logfile
         //Printing the buffer
         /*char *ptr;
-        ptr = strtok(buff, ".");
-        if((strtol(ptr, NULL, 16) < 2018) || (strtol(ptr, NULL, 16) > 1980))*/
-        printf("%s\n",buff);
+        ptr = strtok(buff, "."); */
+
 
 	}
 
@@ -145,18 +201,33 @@ void all_data(char *unfiltered_data, char **data)
     counter++;
 }
 
-void write_logs(char *lines)
+void write_logs(char **data)
 {
     FILE *fileptr;
+    int i;
     fileptr = fopen("temp_log.txt", "w");
     while(!feof(fileptr)) {
-        fprintf(fileptr,"%s\n", lines);
+        fprintf(fileptr,"%s\n", data[i]);
+        i++;
     }
     fclose(fileptr);
 }
 
-int list_data()
+int list_data(char **sorted_data, int size)
 {
-
+    for (int i = 0; i < size; i++) {
+        printf("%s\n", sorted_data[i]);
+    }
     return 0;
+}
+
+void read_logs()
+{
+    FILE *fileptr;
+    char lines[150];
+    fileptr = fopen("temp_log.txt", "r");
+    while(!feof(fileptr)) {
+        fgets(lines, 150, fileptr);
+        printf("%s\n", lines);
+    }
 }
--- week-07/day-4/temp_logger/BlackBox/parser.h
+++ week-07/day-4/temp_logger/BlackBox/parser.h
@@ -9,9 +9,10 @@ int get_port_name();
 int open_port();
 int close_port();
 int log_data();
-int list_data();
+int list_data(char**, int);
 void all_data(char*,char**);
-void write_logs(char*);
+void write_logs(char**);
+void read_logs();
 
 
 /*char *temp_lines;
